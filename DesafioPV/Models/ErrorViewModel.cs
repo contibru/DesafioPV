09e685c1abdb430e13fa606266cd436b6b3d4c03
Bruno Leonardo Conti <bruno.conti@sances.com.br>
2020-01-06 10:55:23 -0300

    First commit

---
+++ DesafioPV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesafioPV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
